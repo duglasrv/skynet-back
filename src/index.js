6a893553874a9b0ec02542e9069d655078a3ebfd
duglasrv <omarrivera123456789@gmail.com>
2025-11-07 17:20:41 -0600

    FIX: Reinstall puppeteer but keep routes disabled to prevent crash

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ const userRoutes = require('./routes/userRoutes');
 const clientRoutes = require('./routes/clientRoutes');
 const visitRoutes = require('./routes/visitRoutes');
 const dashboardRoutes = require('./routes/dashboardRoutes');
-const reportRoutes = require('./routes/reportRoutes');
+//const reportRoutes = require('./routes/reportRoutes');
 
 const app = express();
 
@@ -44,7 +44,7 @@ app.use('/api/users', userRoutes);
 app.use('/api/clients', clientRoutes);
 app.use('/api/visits', visitRoutes);
 app.use('/api/dashboard', dashboardRoutes);
-app.use('/api/reports', reportRoutes);
+//app.use('/api/reports', reportRoutes);
 
 // --- MANEJO DE RUTA 404 ---
 app.use((req, res) => {
